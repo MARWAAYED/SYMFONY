b27299aee6c1273c4328b2a5d412a1537e366617
marwa <marwaayed74@gmail.com>
2022-03-13 21:43:52 +0100

    therd push

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -6,7 +6,6 @@ use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -14,9 +13,8 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(AdRepository $repo, SessionInterface $session): Response
+    public function index(AdRepository $repo): Response
     {
-        dump($session);
         //$repo = $this->getDoctrine()->getRepository(Ad::class);
 
         $ads = $repo->findAll();
@@ -24,5 +22,22 @@ class AdController extends AbstractController
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
         ]);
+    }
+
+
+    /**
+     * permet d'afficher une seule annance
+     * 
+     * @Route("/ads/{slug}",name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo){
+        //recuperation d'une annance
+        $Ad = $repo->findOneBySlug($slug); 
+
+        return $this->render('ad/show.html.twig',['ad'=>$Ad]);
+
+
     }
 }
